d85d7673a104dab7a99f6b0f61295185aa3fe063
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-16 21:39:53 +0300

    done Task5

--- src/Pirogova/Lesson3/Task5.java
+++ src/Pirogova/Lesson3/Task5.java
@@ -3,13 +3,18 @@ package Pirogova.Lesson3;
 public class Task5 {
     public static void main(String[] args) {
         int a = -1;
-        int b = 2;
-        int c = 0;
-        int n = 0; //количество положительных чисел; число 0 не учитывалось за положительное
-        if(a>0){n++;}
-        if(b>0){n++;}
-        if(c>0){n++;}
-        System.out.println(n);
+        int b = -22;
+        int c = -214;
+        int p = 0; //количество положительных чисел; 0 считали положительным
+        int n = 0; //количество отрицательных чисел; число 0 не учитывалось за положительное
+        if(a>0){p++;}
+        else{n++;}
+        if(b>0){p++;}
+        else{n++;}
+        if(c>0){p++;}
+        else{n++;}
+        System.out.println("Количество положительных чисел " + p);
+        System.out.println("Количество отрицательных чисел " + n);
 
     }
 }
