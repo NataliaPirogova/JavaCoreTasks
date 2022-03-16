5e0440a65af1f3ba0eb7a60e25c860d88be00a46
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-16 22:27:15 +0300

    v1 Task1

--- src/Pirogova/Lesson4/Task1.java
+++ src/Pirogova/Lesson4/Task1.java
@@ -2,6 +2,14 @@ package Pirogova.Lesson4;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int d = 1; //количество дней
+        int l = 10; //расстояние в первый день
+        int s = 0; //суммарное расстояние
+        do {
+            s+=l;
+            l*=1.1;
+            d++;
+        }while(d==7);
+        System.out.println("За 7 дней спортсмен пробежал " + s + " километров");
     }
 }
