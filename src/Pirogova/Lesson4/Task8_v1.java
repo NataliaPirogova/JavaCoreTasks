849ba2216fd4c90d9e31540e0a5b9b28c2376b60
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-03-20 20:10:20 +0300

    branch Task8_v1

--- src/Pirogova/Lesson4/Task8_v1.java
+++ src/Pirogova/Lesson4/Task8_v1.java
@@ -2,6 +2,7 @@ package Pirogova.Lesson4;
 
 public class Task8_v1 {
     public static void main(String[] args) {
-
+        int [][]a = {,,,*},{,,*,*},{,*,*,*},{*,*,*,*};
+        System.out.print([][]a);
     }
 }
